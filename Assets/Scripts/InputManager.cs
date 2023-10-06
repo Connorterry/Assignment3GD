762d08e82a68ff63a98459ba32c8b784e653b31e
Connorterry <conna.terry@gmail.com>
2023-10-06 16:02:44 +1100

    moving at linear pace, need to add music

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,37 +1,111 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject item;
-    private Tweener tweener;
-    public Vector3 aEndPos = new Vector3(-2.0f,0.5f,0.0f);
-    public Vector3 dEndPos = new Vector3(2.0f,0.5f,0.0f);
-    public Vector3 sEndPos = new Vector3(0f,0.5f,-2.0f);
-    public Vector3 wEndPos = new Vector3(0f,0.5f,2.0f);
+    [SerializeField] private List<GameObject> itemList = new List<GameObject>();
 
+    private Tweener tweener;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        tweener = GetComponent<Tweener>();
+  
+        tweener  = GetComponent<Tweener>();
+        itemList.Add(item);
+        StartCoroutine(waitSeconds(3.5f));
+    }
+
+    IEnumerator waitSeconds(float i){ // Just waiting for intro music to end
+        yield return new WaitForSeconds(i);
+        StartCoroutine(MovePacStudent());
+    }
+
+    IEnumerator MovePacStudent(){
+        while(true){
+        Vector3 [] endPos = new Vector3 []{
+        new Vector3(-3.432f,3.981f,0.0f), // top left
+        new Vector3(-0.23199f,3.981f,0.0f), // top right
+        new Vector3(-0.23199f,1.421f,0.0f), // bottom right
+        new Vector3(-3.432f,1.421f,0.0f) // bottom left
+        };
+        
+        for(int j=0;j<4;j++) {
+            float duration = Vector3.Distance(item.transform.position,endPos[j])/2.0f; 
+            // ^^ Linear movement calculation based on distance between start and endpoints rather than duration
+            //float duration = 1.5f;
+            switchAnim(j);
+            tweener.AddTween(item.transform,item.transform.position,endPos[j],duration);
+            yield return new WaitForSeconds(duration);
+            }
+        }
+
+    }
+
+    public void switchAnim(int i){
+            switch (i) {
+        case 0:
+            animator.SetTrigger("MovingUp");
+            break;
+        case 1:
+            animator.SetTrigger("MovingRight");
+            break;
+        case 2:
+            animator.SetTrigger("MovingDown");
+            break;
+        case 3:
+            animator.SetTrigger("MovingLeft");
+            break;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("a")){
-            Tweener.AddTween(item.transform, item.transform.position, aEndPos,1.5f);
+        
+        if(Input.GetKeyDown(KeyCode.W)){
+            Vector3 endPos = new Vector3(0.0f,0.5f,2.0f);
+            float duration = 1.5f;
+            foreach(GameObject i in itemList){
+                if(tweener.AddTween(i.transform,i.transform.position,endPos,duration)){
+                    break;
+                }
+            }
         }
-        if(Input.GetKeyDown("d")){
-            Tweener.AddTween(item.transform, item.transform.position, dEndPos,1.5f);
+        if(Input.GetKeyDown(KeyCode.A)){
+            Vector3 endPos = new Vector3(-2.0f,0.5f,0.0f);
+            float duration = 1.5f;
+            foreach(GameObject i in itemList){
+                if(tweener.AddTween(i.transform,i.transform.position,endPos,duration)){
+                    break;
+                }
+            }
         }
-        if(Input.GetKeyDown("s")){
-            Tweener.AddTween(item.transform, item.transform.position, sEndPos,1.5f);
+        if(Input.GetKeyDown(KeyCode.S)){
+            Vector3 endPos = new Vector3(0.0f,0.5f,-2.0f);
+            float duration = 1.5f;
+            foreach(GameObject i in itemList){
+                if(tweener.AddTween(i.transform,i.transform.position,endPos,duration)){
+                    break;
+                }
+            }
         }
-        if(Input.GetKeyDown("w")){
-            Tweener.AddTween(item.transform, item.transform.position, wEndPos,1.5f);
+        if(Input.GetKeyDown(KeyCode.D)){
+            Vector3 endPos = new Vector3(2.0f,0.5f,0.0f);
+            float duration = 1.5f;
+            foreach(GameObject i in itemList){
+                if(tweener.AddTween(i.transform,i.transform.position,endPos,duration)){
+                    break;
+                }
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject newClone = Instantiate(item, new Vector3(0.0f,0.5f,0.0f),Quaternion.identity);
+            itemList.Add(newClone);
         }
     }
 }
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -1,25 +1,67 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tweener : MonoBehaviour
 {
-    private static Tween activeTween;
+    //private Tween activeTween;
+    private List<Tween> activeTweens;
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeTweens = new List<Tween>();
     }
 
     // Update is called once per frame
-    void Update()
+        public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
     {
-        
-    }
-
-    public static void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration){
-        if(activeTween == null){
-            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+        if(!TweenExists(targetObject)){
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+            return true;
+        }
+        else{
+            return false;
         }
     }
+
+    public bool TweenExists(Transform target){
+        foreach (Tween tween in activeTweens){
+            if(tween.Target == target){
+                return true;
+            }
+        }
+        return false;
+    }
+    void Update()
+    {
+
+        if(activeTweens!=null){
+            for (int i = activeTweens.Count - 1; i >= 0; i--){
+
+                Tween tween = activeTweens[i];
+
+                if(Vector3.Distance(tween.Target.position,tween.EndPos)>0.1f){
+                float t = (Time.time - tween.StartTime) / tween.Duration;
+
+                //t = t*t*t; don't need this... movement linear...
+
+                Vector3 nextPos = Vector3.Lerp(tween.StartPos, tween.EndPos, t);
+                tween.Target.position = nextPos;
+                }
+                else
+                {
+                    tween.Target.position = tween.EndPos;
+                    activeTweens.RemoveAt(i);
+                }
+            }
+        }
+    }
+    
+
+
+
+    //public float EaseInCubicFunc(float time) {
+        // return time * time * time;
+    //}
+
 }
